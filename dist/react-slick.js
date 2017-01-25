0eaa34efc58b64eaed52ed199efe75afbd1a521d
d.rokalo <d.rokalo@artox.com>
2017-01-25 18:43:36 +0300

    фикс пролагивания свайпа

--- dist/react-slick.js
+++ dist/react-slick.js
@@ -301,14 +301,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	  // елси пришел только обдейт стилей трека но изменяем на живом доме для скорости и исключения пропедаливания
 	  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
-	    // if(nextProps === this.props && nextState !== this.state && this.state.currentSlide === nextState.currentSlide &&
-	    //   nextState.trackStyle !==  this.state.trackStyle) {
-	    //   const domNode = this.track.getDOMNode();
-	    //   domNode.style.transform = nextState.trackStyle.transform;
-	    //   domNode.style.opacity = nextState.trackStyle.opacity;
-	    //   domNode.style.transition = nextState.trackStyle.transition;
-	    //   return false;
-	    // }
+	    if (nextProps === this.props && nextState !== this.state && this.state.currentSlide === nextState.currentSlide && nextState.trackStyle !== this.state.trackStyle) {
+	      var domNode = this.track.getDOMNode();
+	      domNode.style.transform = nextState.trackStyle.transform;
+	      domNode.style.opacity = nextState.trackStyle.opacity;
+	      domNode.style.transition = nextState.trackStyle.transition;
+	      return false;
+	    }
 	    return true;
 	  },
 
