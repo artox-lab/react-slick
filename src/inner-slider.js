8e7bdaad781cbaaa238df4e5a6261ebd7067824d
d.rokalo <d.rokalo@artox.com>
2017-01-25 19:25:06 +0300

    фикс пролагивания свайпа

--- src/inner-slider.js
+++ src/inner-slider.js
@@ -69,11 +69,10 @@ export var InnerSlider = React.createClass({
   // елси пришел только обдейт стилей трека но изменяем на живом доме для скорости и исключения пропедаливания
   shouldComponentUpdate(nextProps, nextState) {
     if(nextProps === this.props && nextState !== this.state && this.state.currentSlide === nextState.currentSlide &&
-      nextState.trackStyle !==  this.state.trackStyle) {
+      nextState.trackStyle !==  this.state.trackStyle && !domNode.style.transition) {
       const domNode = this.track.getDOMNode();
       domNode.style.transform = nextState.trackStyle.transform;
       domNode.style.opacity = nextState.trackStyle.opacity;
-      domNode.style.transition = nextState.trackStyle.transition;
       return false;
     }
     return true;
